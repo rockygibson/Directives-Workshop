59105f9fef4c9390bedb549f63eafdb0942fbfea
Rocky <rockygibson@gmail.com>
2015-02-25 18:23:55 -0700

    done

--- mainCtrl.js
+++ mainCtrl.js
@@ -1,10 +1,10 @@
 var app = angular.module('directiveWorkshop', ['myDirectives']);
 
-app.controller('mainCtrl', function($scope, mainService){
-$scope.getData = function () {
-        return mainService.getData($scope.query).then(function (data) {
-           console.log(data);
-           return $scope.data = data;
+app.controller('mainCtrl', function($scope, mainService) {
+    $scope.getData = function() {
+        return mainService.getData($scope.query).then(function(data) {
+            console.log(data);
+            return $scope.data = data;
         });
     };
 
